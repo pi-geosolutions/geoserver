0e87efd0f0de139534cf7834ee8f5fd509112840
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2016-04-01 14:37:57 +0200

    [Template Editor] Updated the copyright headers

--- src/community/template-editor/src/main/java/org/geoserver/fmte/web/AbstractTemplateFormPanel.java
+++ src/community/template-editor/src/main/java/org/geoserver/fmte/web/AbstractTemplateFormPanel.java
@@ -1,3 +1,8 @@
+/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+* This code is licensed under the GPL 2.0 license, available at the root
+* application directory.
+*/
+
 package org.geoserver.fmte.web;
 
 import java.util.logging.Logger;
--- src/community/template-editor/src/main/java/org/geoserver/fmte/web/LayerProvider.java
+++ src/community/template-editor/src/main/java/org/geoserver/fmte/web/LayerProvider.java
@@ -1,8 +1,8 @@
 /* (c) 2014 Open Source Geospatial Foundation - all rights reserved
- * (c) 2001 - 2013 OpenPlans
- * This code is licensed under the GPL 2.0 license, available at the root
- * application directory.
- */
+* This code is licensed under the GPL 2.0 license, available at the root
+* application directory.
+*/
+
 package org.geoserver.fmte.web;
 
 import static org.geoserver.catalog.Predicates.sortBy;
